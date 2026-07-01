ffc40b2c8de535f1a76fd005c7606fb1cd7dc319
Satoru <sayochemo@gmail.com>
2026-07-01 19:52:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ tg.ready();
 tg.MainButton.color = '#2481cc';
 tg.MainButton.textColor = '#ffffff';
 
-const btn = document.getElementById("send-btn");
 const familyInput = document.getElementById("family");
 const timeInput = document.getElementById("time");
 
@@ -24,23 +23,7 @@ function checkInputs() {
 familyInput.addEventListener('input', checkInputs);
 timeInput.addEventListener('input', checkInputs);
 
-// Обработка кнопки из самого интерфейса WebApp (если пользователь не использует MainButton от TG)
-btn.addEventListener("click", () => {
-    let family = familyInput.value.trim();
-    let time = timeInput.value.trim();
-    
-    if (family === "" || time === "") {
-        tg.showAlert("Пожалуйста, заполните оба поля!");
-        return;
-    }
-    
-    let data = {
-        family: family,
-        time: time
-    };
-    
-    tg.sendData(JSON.stringify(data));
-});
+
 
 // Обработка нажатия на MainButton самого Telegram
 Telegram.WebApp.onEvent('mainButtonClicked', function() {
